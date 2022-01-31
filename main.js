7c9aae0e6bcd71bf9efa6d6d8277bfb7b78c4405
Abbi Engel <abbi.engel33@gmail.com>
2022-01-31 11:04:37 -0600

    Working with numbers

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import input from 'input'
 
-async function main() {
-    const one = await input.text('Enter a number');
-    const two = await input.text("Enter another number");
-    const numOne = Number(one);
-    const numTwo = Number(two);
+function isEven(num) {
+    if (num % 2 == 0) {
+        console.log(`${num} is even.`);
+    }
+    else {
+        console.log(`${num} is odd.`);
+    }
 
-    console.log(`${numOne} + ${numTwo} = ${numOne + numTwo}`);
-    console.log(`${numOne} - ${numTwo} = ${numOne - numTwo}`);
-    console.log(`${numOne} * ${numTwo} = ${numOne * numTwo}`);
-    console.log(`${numOne} / ${numTwo} = ${numOne / numTwo}`);
-    console.log(`${numOne} % ${numTwo} = ${numOne % numTwo}`);
-    console.log(`${numOne} ** ${numTwo} = ${numOne ** numTwo}`);
+}
+
+async function main() {
+    const maxStr = await input.text('Enter a number');
+    const max = Number(maxStr);
+    let i = 0;
+
+    while (i< max) {
+        isEven(i);
+        i += 1;
+    }
 }
 
 main();
